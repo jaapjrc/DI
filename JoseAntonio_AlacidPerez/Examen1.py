1791df301e85690827b16597770a22eeac943e95
José Antonio Alacid Pérez <joseantonio.alacidp@gmail.com>
2023-12-01 10:04:39 +0100

    Examen Acciones y Eventos

---
+++ JoseAntonio_AlacidPerez/Examen1.py
@@ -0,0 +1,55 @@
+﻿import sys
+from PySide6.QtWidgets import (
+QApplication, QMainWindow, QWidget, QFormLayout, QVBoxLayout,
+QLabel, QLineEdit, QPushButton
+)
+
+class VentanaPrincipal(QMainWindow) :
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Registro de empleados")
+        self.setMinimumWidth(300)
+        layout_vertical = QVBoxLayout()
+        self.layout_labels = QVBoxLayout()
+        registrar = QPushButton("Registrar")
+        registrar.clicked.connect(self.registrarEmpleado)
+        label = QLabel("Empleados Registrados:")
+        layout_formulario = QFormLayout()
+        componente_principal = QWidget()
+        componente_principal.setLayout(layout_vertical)
+        layout_vertical.addLayout(layout_formulario)
+        self.setCentralWidget(componente_principal)
+        self.nombre = QLineEdit()
+        layout_formulario.addRow(QLabel("Nombre: "), self.nombre)
+        self.edad = QLineEdit()
+        layout_formulario.addRow(QLabel("Edad: "), self.edad)
+        self.direccion = QLineEdit()
+        layout_formulario.addRow(QLabel("Dirección: "), self.direccion)
+        self.telefono = QLineEdit()
+        layout_formulario.addRow(QLabel("Teléfono: "), self.telefono)
+        self.cargo = QLineEdit()
+        layout_formulario.addRow(QLabel("Cargo: "), self.cargo)
+        self.salario = QLineEdit()
+        layout_formulario.addRow(QLabel("Salario: "), self.salario)
+        layout_vertical.addWidget(registrar)
+        layout_vertical.addWidget(label)
+        layout_vertical.addLayout(self.layout_labels)
+        
+    def registrarEmpleado(self):
+        if self.nombre.text() != "" and self.cargo.text() != "" and self.salario.text() != "":
+            label = QLabel()
+            label.setText(f"{self.nombre.text()} - {self.cargo.text()} - {self.salario.text()}")
+            self.layout_labels.addWidget(label)
+            self.nombre.clear()
+            self.edad.clear()
+            self.direccion.clear()
+            self.telefono.clear()
+            self.cargo.clear()
+            self.salario.clear()
+        
+        
+if __name__ == "__main__":
+    app = QApplication([])
+    ventana = VentanaPrincipal()
+    ventana.show()
+    app.exec()
---
+++ JoseAntonio_AlacidPerez/Examen2.py
@@ -0,0 +1,58 @@
+﻿import os
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction, QIcon, QKeySequence
+from PySide6.QtWidgets import (
+QApplication, QMainWindow, QToolBar, QWhatsThis, QDockWidget, QLabel,
+QTextEdit
+)
+
+class VentanaPrincipal(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("¿Cual es la asignatura mas importante?")
+        self.setMinimumSize(400,400)
+        barra_menus = self.menuBar()
+        menu = barra_menus.addMenu("&Asignaturas")
+        
+        rutas = {
+            "libros": os.path.join(os.path.dirname (__file__), "libros.png"),
+            "ayuda": os.path.join(os.path.dirname (__file__), "ayuda.png")
+            }
+        
+        accion = QAction(QIcon(rutas["libros"]), "Prioridad de asignaturas \n 2ºDAM", self)
+        accion.setWhatsThis("Nos indica cual es la asignatura mas importante de 2ºDAM")
+        accion.setShortcut(QKeySequence("Ctrl+p"))
+        accion.triggered.connect(self.imprimir_por_dock)
+        
+        accion1 = QAction(QIcon(rutas["ayuda"]), "Ayuda", self)
+        accion1.setWhatsThis("Activamos el modo ayuda")
+        accion1.setShortcut(QKeySequence("Shift+F1"))
+        accion1.triggered.connect(self.entrar_whatsthis)
+        
+        menu.addActions([accion,accion1])
+        
+        barra_herramientas = QToolBar("Barra de herramientas 1")
+        barra_herramientas.addActions([accion, accion1])
+        barra_herramientas.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        self.addToolBar(barra_herramientas)
+        
+        dock1 = QDockWidget()
+        dock1.setWindowTitle("El subsconsciente del alumno")
+        self.texto_dock = QTextEdit("")
+        dock1.setWidget(self.texto_dock)
+        self.addDockWidget(Qt.TopDockWidgetArea, dock1)
+        
+    def imprimir_por_dock(self):
+        self.texto_dock.append("La asignatura mas importante de 2ºDAM es Desarrollo de Interfaces")
+        
+    def entrar_whatsthis(self):
+        if (QWhatsThis.inWhatsThisMode()):
+            QWhatsThis.leaveWhatsThisMode()
+        else:
+            QWhatsThis.enterWhatsThisMode()
+            
+if __name__ == "__main__":
+    app = QApplication([])
+    ventana = VentanaPrincipal()
+    ventana.show()
+    app.exec()
